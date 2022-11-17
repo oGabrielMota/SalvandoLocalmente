92437ffdcfea34682ff41b68cf7c8efb86301258
Gabriel Mota <100182257+oGabrielMota@users.noreply.github.com>
2022-11-17 12:39:00 -0300

    Subindo Projeto completo

---
+++ App.js
@@ -0,0 +1,39 @@
+import { FlatList, SafeAreaView, StatusBar, StyleSheet } from "react-native"
+import NotaEditor from "./src/componentes/NotaEditor"
+import { Nota } from "./src/componentes/Nota"
+import { useEffect, useState } from "react"
+import { criaTabela } from "./src/servicos/Notas"
+
+export default function App() {
+
+  useEffect(() => {
+    criaTabela()
+  }, [])
+
+  const [notas, setNotas] = useState([])
+
+  async function mostraNotas() {
+    setNotas(todasNotas)
+    console.log(todasNotas)
+  }
+
+  return (
+    <SafeAreaView style={estilos.container}>
+      <FlatList
+        data={notas}
+        renderItem={(nota) => <Nota {...nota}/>}
+        keyExtractor={nota => nota[0]}/>
+      <NotaEditor mostraNotas={mostraNotas}/>
+      <StatusBar/>
+    </SafeAreaView>
+  )
+}
+
+const estilos = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "stretch",
+		justifyContent: "flex-start",
+	},
+})
+
